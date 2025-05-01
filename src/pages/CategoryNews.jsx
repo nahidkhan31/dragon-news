ad6ad618d80f8dadfeb725a9c756227df21ce55c
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-05-01 19:10:15 +0600

    added eight vedio

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router";
+import NewsCard from "../components/NewsCard";
 
 const CategoryNews = () => {
   const { id } = useParams();
@@ -25,7 +26,15 @@ const CategoryNews = () => {
 
   return (
     <div>
-      <h2>Total {categoryNews.length} News Found</h2>
+      <h2 className="font-bold mb-5">
+        Total <span className="text-secondary">{categoryNews.length}</span> News
+        Found
+      </h2>
+      <div className="grid grid-cols-1">
+        {categoryNews.map((news) => (
+          <NewsCard key={news.id} news={news}></NewsCard>
+        ))}
+      </div>
     </div>
   );
 };
