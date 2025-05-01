69e02388b99a8642a7523156c42845e40ed1edcf
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-05-01 19:50:54 +0600

    finished 52 module

--- src/components/HomeLayout/FindUs.jsx
+++ src/components/HomeLayout/FindUs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router";
 
 const FindUs = () => {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Navigate } from "react-router";
 
 const Home = () => {
   return (
     <div>
-      <h2>home page</h2>
+      <Navigate to="/category/1"></Navigate>
     </div>
   );
 };
