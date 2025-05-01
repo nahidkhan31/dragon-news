d10f8ae5fc23c804e3458e4ee360b5602c7f1562
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-05-01 13:49:08 +0600

    compleate four vedio

--- src/layoutes/HomeLayout.jsx
+++ src/layoutes/HomeLayout.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
   return (
@@ -8,6 +10,12 @@ const HomeLayout = () => {
       <header>
         <Header></Header>
       </header>
+      <section className="w-11/12 mx-auto my-3">
+        <LatestNews></LatestNews>
+      </section>
+      <nav className="w-11/12 mx-auto my-3">
+        <Navbar></Navbar>
+      </nav>
       <main>
         <section className="left-nav"></section>
         <section className="main-nav">
